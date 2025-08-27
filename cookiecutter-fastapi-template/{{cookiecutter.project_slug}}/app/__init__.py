d22e02759b49b4a401d4cda7eba9d6a91f564390
Fiqih Fathor Rachim <fiqih.fathor.rachim@gmail.com>
2025-08-27 16:24:11 +0700

    chore: remove cookiecutter configuration file

--- cookiecutter-fastapi-template/{{cookiecutter.project_slug}}/app/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-{{ cookiecutter.project_name }} - FastAPI Application
-"""
-
-__version__ = "{{ cookiecutter.version }}"
--- cookiecutter-fastapi-template/{{cookiecutter.project_slug}}/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-{% if cookiecutter.use_pytest == "y" %}
-"""
-Test package for the project.
-"""
-{% endif %}
